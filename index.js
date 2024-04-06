d7bcbd04b04afb73788fb8af883bd4624bc2be02
cutePanda123 <peter.li.dev@outlook.com>
2024-04-05 21:00:06 -0700

    add brower page open timeout

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const queryTimes = 30;
-const waitTime = 30000;   // wait for 30 seconds between two requests
+const waitTime = 10000;   // wait for 30 seconds between two requests
 const url = 'https://www.redfin.com/VA/Vienna/2710-Earls-Ct-22181/home/9527207';
 (async () => {
   for (var i = 0; i < queryTimes; i++) {
@@ -9,8 +9,13 @@ const url = 'https://www.redfin.com/VA/Vienna/2710-Earls-Ct-22181/home/9527207';
         headless: false
       });
       const page = await browser.newPage();
-      await page.goto(url);
+      await page.goto(url, { timeout: 60000 });
+      console.log("open a new page")
+      await new Promise(r => setTimeout(r, waitTime));
+
+      console.log("close brower")
       await browser.close();
+
       await new Promise(r => setTimeout(r, waitTime));
     } catch (e) {
       console.log('ignore issue and retry: ' + e);
